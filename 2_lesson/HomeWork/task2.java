78917264b555b67a42fb56f0d3da5b755451e51f
Alexey <lex1645@yandex.ru>
2023-03-26 17:29:01 +0300

    Done HW

--- 2_lesson/HomeWork/task2.java
+++ 2_lesson/HomeWork/task2.java
@@ -3,13 +3,16 @@ package HomeWork;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 
 public class task2 {
-    public static void main(String[] args) throws IOException{
-       
-        Path of = Path.of("file.txt");
-        System.out.println(Files.exists(of));
+    public static void main(String[] args) throws IOException {
 
-        Files.createFile(of);
+        Path dir = Path.of("file.txt");
+        String text = "TEXT\n";
+
+        for (int i = 0; i < 100; i++) {
+            Files.writeString(dir, text, StandardOpenOption.APPEND);
+        }
     }
 }
