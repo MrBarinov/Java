50028b9e8652965b2e642d07397de36bc5e4d5e7
Alexey <lex1645@yandex.ru>
2023-03-26 16:40:58 +0300

    Done task1

--- 2_lesson/HomeWork/task1.java
+++ 2_lesson/HomeWork/task1.java
@@ -1,10 +1,17 @@
 package HomeWork;
-
 import java.io.IOException;
 
 public class task1 {
     public static void main(String[] args) throws IOException {
-        // System.out.println(buildString(6, "a", "b" ));
+        System.out.println(buildString(6, 'a', 'b'));
     }
-    
+
+    static String buildString(int n, char c1, char c2) {
+        String str = new String();
+        for (int i = 0; i < n; i++) {
+            str += (i % 2 == 0) ? c1 : c2;
+        }
+        return str;
+
+    }
 }
